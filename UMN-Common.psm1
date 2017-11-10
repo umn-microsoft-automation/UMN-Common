84ebfad9c4f33725df586803b327ae3c464a3276
tjsobeck <tjsobeck@umn.edu>
2017-11-10 10:29:16 -0600

    add function Get-WebReqErrorDetails
    bump up module version as well

--- UMN-Common.psm1
+++ UMN-Common.psm1
@@ -322,6 +322,34 @@ function Get-RandomString {
 	return $Result
 } #END Get-RandomString
 
+#region Get-WebReqErrorDetails
+function Get-WebReqErrorDetails {
+	<#
+		.SYNOPSIS
+			Returns JSON Responsbody data from an Error thrown by Invoke-Webrequest or Invoke-RestMethod
+
+		.DESCRIPTION
+			Returns JSON Responsbody data from an Error thrown by Invoke-Webrequest or Invoke-RestMethod
+
+		.PARAMETER err
+			Error thrown by Invoke-Webrequest or Invoke-RestMethod
+
+		.NOTES
+			Author: Travis Sobeck
+	#>
+	[CmdletBinding()]
+	param
+	(
+		[Parameter(Mandatory)]
+		[System.Management.Automation.ErrorRecord]$err
+	)
+	
+	$reader = New-Object System.IO.StreamReader($err.Exception.Response.GetResponseStream())
+	$reader.BaseStream.Position = 0
+	$reader.DiscardBufferedData()
+	return ($reader.ReadToEnd() | ConvertFrom-Json)
+}
+#endregion
 function Out-RecursiveHash {
 	<#
 		.SYNOPSIS
